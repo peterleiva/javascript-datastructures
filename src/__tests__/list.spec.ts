7083248b21aad9d42c1a5163b841c39bffdeacd0
Peter Leiva <pleiva@id.uff.br>
2020-10-01 07:25:36 -0300

    Unit test list

--- src/__tests__/list.spec.ts
+++ src/__tests__/list.spec.ts
@@ -21,9 +21,20 @@ describe('List', () => {
 			});
 		});
 
+		describe('With list of argument', () => {
+			it('Add in correct order they are called', () => {
+				list.append(1, 2);
+				expect([...list]).toEqual([1, 2]);
+			});
+
+			it('Increase by the length of items added', () => {
+				list.append(1, 2, 3);
+				expect(list.length).toEqual(3);
+			});
+		});
 		describe('without empty list', () => {
 			beforeEach(() => {
-				list.append(1).append(2);
+				list.append(1, 2);
 			});
 
 			it('Insert at the end', () => {
@@ -66,6 +77,90 @@ describe('List', () => {
 				expect([...list]).toEqual([2, 1]);
 			});
 		});
+
+		describe('With list as argument', () => {
+			it('Prepend list as order of call', () => {
+				list.prepend(1, 2, 3);
+				expect([...list]).toEqual([1, 2, 3]);
+			});
+
+			it('Insert at beggining for already inserted items', () => {
+				list.append(4);
+				list.prepend(1, 2, 3);
+				expect([...list]).toEqual([1, 2, 3, 4]);
+			});
+
+			it('Increase length by added size', () => {
+				list.prepend(1, 2, 3);
+				expect(list.length).toBe(3);
+			});
+		});
+	});
+
+	describe('.keys', () => {
+		it('Get empty array when list is empty', () => {
+			expect([...list.keys()]).toEqual([]);
+		});
+
+		it('Get list of index of stored items', () => {
+			list.append(1, 2, 3);
+			expect([...list.keys()]).toEqual([0, 1, 2]);
+		});
+
+		it('Iterate over indexes', () => {
+			let index = 0;
+			list.append(1, 2, 3);
+			for (const i of list.keys()) {
+				expect(i).toBe(index);
+				index++;
+			}
+		});
+	});
+
+	describe('.values', () => {
+		it('Get empty array when list is empty', () => {
+			expect([...list.values()]).toEqual([]);
+		});
+
+		it('Get list of items', () => {
+			list.append(1, 2, 3);
+			expect([...list.values()]).toEqual([1, 2, 3]);
+		});
+
+		it('Iterate over values', () => {
+			let index = 0;
+			list.append(1, 2, 3);
+			for (const value of list.values()) {
+				expect(value).toBe(list.get(index));
+				index++;
+			}
+		});
+	});
+
+	describe('.entries', () => {
+		it('Get empty array when list is empty', () => {
+			expect([...list.entries()]).toEqual([]);
+		});
+
+		it('Get list of same stored items', () => {
+			list.append(1, 2, 3);
+
+			expect([...list.entries()]).toEqual([
+				[0, 1],
+				[1, 2],
+				[2, 3],
+			]);
+		});
+
+		it('Iterate over entries of list', () => {
+			let i = 0;
+			list.append(1, 2, 3);
+
+			for (const [index, item] of list.entries()) {
+				expect([index, item]).toEqual([i, list.get(index)]);
+				i++;
+			}
+		});
 	});
 
 	describe('.get', () => {
@@ -99,7 +194,23 @@ describe('List', () => {
 			});
 		});
 
-		describe('Without empty list', () => {
+		describe('single element', () => {
+			beforeEach(() => {
+				list.append(1);
+			});
+
+			it('Remove all elements', () => {
+				list.remove(0);
+				expect([...list]).toEqual([]);
+			});
+
+			it('Get 0 length', () => {
+				list.remove(0);
+				expect(list.length).toBe(0);
+			});
+		});
+
+		describe('More than 1 element', () => {
 			beforeEach(() => {
 				list.append(1).append(2);
 			});
@@ -114,11 +225,114 @@ describe('List', () => {
 				expect(list.remove(2)).toBe(3);
 			});
 
-			it('Remove the element from the list', () => {
+			it('Remove tail element', () => {
+				list.remove(1);
+				expect([...list]).toEqual([1]);
+			});
+
+			it('Remove head element', () => {
+				list.remove(0);
+				expect([...list]).toEqual([2]);
+			});
+
+			it('Remove the between head and tail', () => {
 				list.append(3);
 				list.remove(1);
 				expect([...list]).toEqual([1, 3]);
 			});
+		});
+	});
+
+	describe('.peek', () => {
+		it('Get null when list is empty', () => {
+			expect(list.peek()).toBeNull();
+		});
+
+		it('Get the first element from the list', () => {
+			list.append(1, 2);
+			expect(list.peek()).toBe(list.get(0));
+		});
+
+		it('Get single element stored', () => {
+			list.append(1);
+			expect(list.peek()).toBe(1);
+		});
+
+		it('Do not remove element', () => {
+			list.append(1);
+			list.peek();
+			expect(list.peek()).toBe(1);
+		});
+	});
+
+	describe('.tail', () => {
+		it('Get null when list is empty', () => {
+			expect(list.tail()).toBeNull();
+		});
+
+		it('Get single element stored', () => {
+			list.append(1);
+			expect(list.tail()).toBe(1);
+		});
+
+		it('Get the last element added', () => {
+			list.append(1, 2, 3, 4);
+			expect(list.tail()).toBe(list.get(list.length - 1));
+		});
+
+		it('Do not remove element', () => {
+			list.append(1, 2);
+			list.tail();
+			expect(list.length).toBe(2);
+		});
+	});
+
+	describe('.contains', () => {
+		describe('Returns false When', () => {
+			it('Has List is empty', () => {
+				expect(list.contains(10)).toBe(false);
+			});
+
+			it('Has no item being searched', () => {
+				list.append(1);
+				expect(list.contains(10)).toBe(false);
+			});
+		});
+
+		describe('Returns true when', () => {
+			it('Has item inside list', () => {
+				list.append(1, 2, 3);
+				expect(list.contains(2)).toBe(true);
+			});
+
+			it('Item is at the beginning of list', () => {
+				list.append(1, 2, 3);
+				expect(list.contains(1)).toBe(true);
+			});
+			it('Item is at end', () => {
+				list.append(1, 2, 3);
+				expect(list.contains(3)).toBe(true);
+			});
+
+			it('Has more than 1 instance stored', () => {
+				list.append(1, 2, 3, 2);
+				expect(list.contains(2)).toBe(true);
+			});
+		});
+	});
+
+	describe('.clear', () => {
+		it('get length = 0', () => {
+			list.append(1, 2, 3);
+			list.clear();
+
+			expect(list.length).toBe(0);
+		});
+
+		it('Remove all elements', () => {
+			list.append(1, 2, 3);
+			list.clear();
+			expect([...list]).toEqual([]);
 		});
 	});
 });
