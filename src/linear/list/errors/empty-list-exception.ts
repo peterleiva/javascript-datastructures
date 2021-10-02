e2e3766a3a8c8d5e52f20a9e4a531e49d696c95a
Peter <github@peter.dev.br>
2021-10-02 04:51:50 -0300

    Remove EmptyListException error to use Underflow

--- src/linear/list/errors/empty-list-exception.ts
+++
@@ -1,7 +0,0 @@
-export default class EmptyListException extends Error {
-	constructor() {
-		super();
-
-		this.message = `Can't remove item from a empty list`;
-	}
-}
--- src/linear/list/errors/index.ts
+++ src/linear/list/errors/index.ts
@@ -1,2 +1,1 @@
-export { default as EmptyListException } from "./empty-list-exception";
 export { default as IndexOutOfRangeException } from "./index-out-of-range-exception";
