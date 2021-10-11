45244ad64da3cccf40c337a59755f0a4a75d4276
Peter <github@peter.dev.br>
2021-10-11 11:55:59 -0300

    Remove unnecessary binary tree tests

--- src/tree/binary-tree/__tests__/binary-tree.spec.ts
+++ src/tree/binary-tree/__tests__/binary-tree.spec.ts
@@ -2,11 +2,6 @@ import BinaryTree from "../binary-tree";
 
 describe("Binary Tree", () => {
 	describe("Creating the tree", () => {
-		it("Create a tree successfully", () => {
-			const tree = new BinaryTree();
-			expect(tree).toBeDefined();
-		});
-
 		it("Gets empty root for created tree with no data", () => {
 			const tree = new BinaryTree();
 			expect(tree.root).toBeNull();
@@ -27,43 +22,6 @@ describe("Binary Tree", () => {
 	});
 
 	describe("Methods", () => {
-		describe(".size", () => {
-			it("Gets 0 for newly created tree", () => {
-				const tree = new BinaryTree();
-				expect(tree.size()).toBe(0);
-			});
-
-			it("Gets 1 for root only element", () => {
-				const tree = new BinaryTree(10);
-				expect(tree.size()).toBe(1);
-			});
-
-			it.todo("Gets 2 for single leat right");
-			it.todo("Gets 2 for single leaf right");
-			it.todo("Gets 10 for 10 appends");
-		});
-
-		describe(".isEmpty", () => {
-			describe("Created with no data", () => {
-				let tree: BinaryTree<number>;
-
-				beforeEach(() => {
-					tree = new BinaryTree();
-				});
-
-				it("Returns true", () => {
-					expect(tree.empty()).toBe(true);
-				});
-
-				it.todo("Returns true after inserting at root");
-			});
-
-			it("Gets false for new tree with data", () => {
-				const tree = new BinaryTree(10);
-				expect(tree.empty()).toBe(false);
-			});
-		});
-
 		describe(".find", () => {
 			const dataSearched = 10;
 			const finder = (data: number) => data === dataSearched;
