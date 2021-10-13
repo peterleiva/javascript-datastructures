3cabe667e1dccca56d26096424ab9c3c3876c7a5
Peter <github@peter.dev.br>
2021-10-13 11:01:18 -0300

    Fix binary tree exportables

--- src/tree/binary-tree/index.ts
+++ src/tree/binary-tree/index.ts
@@ -1,5 +1,2 @@
-import BST from "./bst";
-
 export { default as BinaryTree } from "./binary-tree";
-
-export { BST, BST as BinarySearchTree };
+export { default as BST, default as BinarySearchTree } from "./bst";
